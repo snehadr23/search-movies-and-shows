5bfdb0f9b4379d2f75dd7ee999bdd865becb5a7d
Sneha Rajakumar <snehadr23@gmail.com>
2021-04-29 00:00:44 -0700

    Initial commit

---
+++ src/movies/Movies.js
@@ -0,0 +1,12 @@
+import './Movies.css';
+import {useState, useEffect} from 'react';
+
+function Movies() {
+    return(
+        <main className = 'main'>
+            <p>Movies here</p>
+        </main>
+    );
+}
+
+export default Movies;
